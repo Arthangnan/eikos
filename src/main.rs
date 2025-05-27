89596206b9b0853d77bcdf07873b1cd061df48bb
Andrew Attali <me@mortebrume.eu>
2025-05-27 21:31:54 +0200

    nit: remove unecessary loop instruction in main.rs
    we are already endlessly looping with boot.asm for the moment.

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,10 @@
 
 use core::panic::PanicInfo;
 
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {}
-}
-
 static HELLO: &[u8] = b"Hello World!";
 
 #[unsafe(no_mangle)]
-pub extern "C" fn teleia() -> ! {
+pub extern "C" fn teleia() {
     let vga_buffer = 0xb8000 as *mut u8;
 
     for (i, &byte) in HELLO.iter().enumerate() {
@@ -20,6 +15,9 @@ pub extern "C" fn teleia() -> ! {
             *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
         }
     }
+}
 
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
