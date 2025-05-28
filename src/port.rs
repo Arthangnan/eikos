13e7b5a815a58694c420189c1ddeb86d58ae8da1
Andrew Attali <me@mortebrume.eu>
2025-05-28 14:13:07 +0200

    feat: create port struct for read/write

--- src/port.rs
+++ src/port.rs
@@ -1,4 +1,6 @@
 use core::arch::asm;
+use core::fmt;
+use core::marker::PhantomData;
 
 pub trait PortRead {
     unsafe fn read_from_port(port: u16) -> Self;
@@ -67,3 +69,61 @@ impl PortWrite for u32 {
         }
     }
 }
+
+pub struct Port<T> {
+    port: u16,
+    phantom: PhantomData<T>,
+}
+
+impl<T> Port<T> {
+    #[inline]
+    pub const fn new(port: u16) -> Port<T> {
+        Port {
+            port,
+            phantom: PhantomData,
+        }
+    }
+}
+
+// Reading and writing to an I/O port can have side effects.
+// We enforce mutability to align with this behaviour.
+
+impl<T: PortRead> Port<T> {
+    #[inline]
+    pub unsafe fn read(&mut self) -> T {
+        unsafe { T::read_from_port(self.port) }
+    }
+}
+
+impl<T: PortWrite> Port<T> {
+    #[inline]
+    pub unsafe fn write(&mut self, value: T) {
+        unsafe { T::write_to_port(self.port, value) }
+    }
+}
+
+impl<T> fmt::Debug for Port<T> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("Port")
+            .field("port", &self.port)
+            .field("size", &core::mem::size_of::<T>())
+            .finish()
+    }
+}
+
+impl<T> Clone for Port<T> {
+    fn clone(&self) -> Self {
+        Self {
+            port: self.port,
+            phantom: PhantomData,
+        }
+    }
+}
+
+impl<T> PartialEq for Port<T> {
+    fn eq(&self, other: &Self) -> bool {
+        self.port == other.port
+    }
+}
+
+impl<T> Eq for Port<T> {}
